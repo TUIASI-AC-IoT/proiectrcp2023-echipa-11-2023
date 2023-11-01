cc9569dc4e1e5e28b635ebb84464450acb7556bd
student <student@debian-BULLSEYE-live-builder-AMD64>
2023-11-01 12:58:45 +0200

    First commit

---
+++ interface.py
@@ -0,0 +1,31 @@
+
+
+
+#imports
+import tkinter as tk
+
+
+
+#class definitions
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("300x200")
+        self.grid()
+
+
+        #menu frame
+        self.__menu = tk.Frame(self, highlightbackground="black", highlightthickness=1)
+        self.__menu.grid()
+        self.__menu.pack(anchor="n", fill="x")
+
+
+
+
+def main():
+    x = Window()
+    x.title("Testing interface")
+    x.mainloop()
+
+main()
